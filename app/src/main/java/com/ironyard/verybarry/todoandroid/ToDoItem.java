781abd8f6ad31ebbd2fb1dc91250c8053f2d1cde
VeryBarry <barry.a.daniels@gmail.com>
2016-09-28 10:36:04 -0400

    made app

---
+++ app/src/main/java/com/ironyard/verybarry/todoandroid/ToDoItem.java
@@ -0,0 +1,20 @@
+package com.ironyard.verybarry.todoandroid;
+
+/**
+ * Created by VeryBarry on 9/28/16.
+ */
+
+public class ToDoItem {
+    String text;
+    boolean isDone;
+
+    public ToDoItem(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+
+
+
+
+
+}
